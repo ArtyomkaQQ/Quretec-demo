c973b5bc7798ea659c1c951407c46e09955e4b23
Artyom Aralov <artyom.aralov@gmail.com>
2020-04-15 18:20:57 +0300

    Routing not working

--- src/App.js
+++ src/App.js
@@ -1,69 +1,22 @@
 import React, { Component } from 'react';
-import './App.scss';
-
-import { Layout, Menu } from 'antd';
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-} from '@ant-design/icons';
-
-const { Header, Sider, Content } = Layout;
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import Home from './Home'
+import Group from './Group'
+import Manageable from './Manageable'
 
 class App extends Component {
-  state = {
-    collapsed: false,
-  };
-
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
-
-  render() {
-    return (
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <UserOutlined />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <VideoCameraOutlined />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <UploadOutlined />
-              <span>nav 3</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: this.toggle,
-            })}
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
-      </Layout>
-    );
-  }
+    state = {}
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path='/' component={Home} />
+                    <Route path='/grupid' component={Group} />
+                    <Route path='/haldamisel' component={Manageable} />
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/Manageable.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import AppNav from './AppNav';
+
+class Manageable extends Component {
+    render() {
+        return (
+          <div>
+            <AppNav />
+            <h2 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+              Welcome to easy Haldamisel !
+                   </h2>
+          </div>
+        );
+    }
+}
+ 
+export default Manageable;
