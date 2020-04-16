e6396f829afd82d27efcc77b223abb65749f7f7a
Artyom Aralov <artyom.aralov@gmail.com>
2020-04-16 10:41:10 +0300

    Base

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ import {
   AppstoreOutlined,
   PieChartOutlined,
   DesktopOutlined,
-  MailOutlined
+  MailOutlined,
 } from "@ant-design/icons";
-import Group from './Group';
-import Manageable from './Manageable';
-import counterpart from 'counterpart';
-import Translate from 'react-translate-component';
-import ee from './lang/ee';
-import en from './lang/en'
+import Group from "./Group";
+import Manageable from "./Manageable";
+import counterpart from "counterpart";
+import Translate from "react-translate-component";
+import ee from "./lang/ee";
+import en from "./lang/en";
 
 const { Header, Sider, Content } = Layout;
 const { SubMenu } = Menu;
-counterpart.registerTranslations('ee', ee);
-counterpart.registerTranslations('en', en);
-counterpart.setLocale('ee');
+counterpart.registerTranslations("ee", ee);
+counterpart.registerTranslations("en", en);
+counterpart.setLocale("ee");
 
 class AppNav extends Component {
   state = {
@@ -38,7 +38,7 @@ class AppNav extends Component {
   onLangChange = (e) => {
     this.setState({ lang: e.target.value });
     counterpart.setLocale(e.target.value);
-  }
+  };
 
   render() {
     return (
@@ -47,37 +47,41 @@ class AppNav extends Component {
           <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
             <div className="logo" />
             <Menu
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
               mode="inline"
               theme="dark"
               inlineCollapsed={this.state.collapsed}
             >
               <Menu.Item key="2">
                 <PieChartOutlined />
-                <span><Translate content="home" /></span>
+                <span>
+                  <Translate content="home" />
+                </span>
               </Menu.Item>
               <SubMenu
                 key="sub1"
                 title={
                   <span>
                     <MailOutlined />
-                    <span><Translate content="forms.f1" /></span>
+                    <span>
+                      <Translate content="forms.f1" />
+                    </span>
                   </span>
                 }
               >
                 <Menu.Item key="5">
                   <Link to="/grupid">
                     <Translate content="forms.f2" />
-                  </Link></Menu.Item>
+                  </Link>
+                </Menu.Item>
                 <Menu.Item key="6">
                   <Link to="/haldamisel">
-                  <Translate content="forms.f3" />
-                  </Link></Menu.Item>
-                <Menu.Item key="7">
-                <Link to="/">
-                    Avaldamisel
+                    <Translate content="forms.f3" />
                   </Link>
+                </Menu.Item>
+                <Menu.Item key="7">
+                  <Link to="/">Avaldamisel</Link>
                 </Menu.Item>
                 <Menu.Item key="8">Option 8</Menu.Item>
               </SubMenu>
@@ -105,15 +109,27 @@ class AppNav extends Component {
           </Sider>
           <Layout className="site-layout">
             <Header className="site-layout-background" style={{ padding: 0 }}>
-              {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                className: 'trigger',
-                onClick: this.toggle,
-              })}
+              {React.createElement(
+                this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                {
+                  className: "trigger",
+                  onClick: this.toggle,
+                }
+              )}
+            </Header>
+
+            <Header>
+              <div className="logo" />
+              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+              </Menu>
             </Header>
             <Content
               className="site-layout-background"
               style={{
-                margin: '24px 16px',
+                margin: "24px 16px",
                 padding: 24,
                 minHeight: 280,
               }}
@@ -123,8 +139,8 @@ class AppNav extends Component {
                 <option value="en">EN</option>
               </select>
 
-              <Route exact={true} path='/grupid' component={Group} />
-              <Route exact={true} path='/haldamisel' component={Manageable} />
+              <Route exact={true} path="/grupid" component={Group} />
+              <Route exact={true} path="/haldamisel" component={Manageable} />
             </Content>
           </Layout>
         </Layout>
--- src/Group.js
+++ src/Group.js
@@ -1,50 +1,62 @@
-import React, { Component } from 'react';
-import { Button, Tooltip } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-import { Input } from 'antd';
-import counterpart from 'counterpart';
-import Translate from 'react-translate-component';
-import ee from './lang/ee';
-import en from './lang/en';
+import React, { Component } from "react";
+import { Button, Tooltip } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { Input} from "antd";
+import counterpart from "counterpart";
+import Translate from "react-translate-component";
+import ee from "./lang/ee";
+import en from "./lang/en";
 
 const { Search } = Input;
-counterpart.registerTranslations('ee', ee);
-counterpart.registerTranslations('en', en);
-counterpart.setLocale('ee');
+counterpart.registerTranslations("ee", ee);
+counterpart.registerTranslations("en", en);
+counterpart.setLocale("ee");
 
 class Group extends Component {
-    state = { 
-        show: false
-     }
+  state = {
+    show: false,
+  };
 
-    toggle = () => this.setState((currentState) => ({show: !currentState.show}));
+  toggle = () =>
+    this.setState((currentState) => ({ show: !currentState.show }));
 
-    onLangChange = (e) => {
-        this.setState({lang: e.target.value});
-        counterpart.setLocale(e.target.value);
-      }
+  onLangChange = (e) => {
+    this.setState({ lang: e.target.value });
+    counterpart.setLocale(e.target.value);
+  };
 
-    render() { 
-        return (
-            <div>
-                <Tooltip title="search">
-                    <Button type="primary" shape="circle" icon={<SearchOutlined />} onClick={this.toggle}></Button>
-                    {this.state.show && <Search
-                        placeholder="input search text"
-                        onSearch={value => console.log(value)}
-                        style={{ width: 200, marginLeft: 10 }}
-                    />}
-                </Tooltip>
-                <div>
+  render() {
+    return (
+      <div>
+        <Tooltip title="search">
+          <Button
+            type="primary"
+            shape="circle"
+            icon={<SearchOutlined />}
+            onClick={this.toggle}
+          ></Button>
+          {this.state.show && (
+            <Search
+              placeholder="input search text"
+              onSearch={(value) => console.log(value)}
+              style={{ width: 200, marginLeft: 10 }}
+            />
+          )}
+        </Tooltip>
+        <div></div>
 
-                </div>
-
-                <h2 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Translate content="title" />
-            </h2>
-            </div>
-        );
-    }
+        <h2
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Translate content="title" />
+        </h2>
+      </div>
+    );
+  }
 }
- 
+
 export default Group;
